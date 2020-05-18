3a0bb7e00b7566ee17c6e505d7656a943693f4ea
Zane Finner <zanefinnergm@gmail.com>
2020-05-18 15:38:36 -0400

    index finished

--- resources/views/communities/index.blade.php
+++ resources/views/communities/index.blade.php
@@ -1,6 +1,9 @@
-<!doctype HTML>
-<html>
+<!DOCTYPE html>
+<html lang="en">
   <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Communities</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="/common.css">
   </head>
@@ -13,16 +16,18 @@
         <input type="" placeholder="" name=""/>
         <button type="submit">Add</button>
     </form>
+    @foreach($data as $key)
       <div class="row">
         <div class="col-6 info">
-          <h2>Name</h2>
-          <p>Description</p>
+          <h2>{{$key->name}}</h2>
+          <p>{{$key->about}}</p>
         </div>
         <div class="col-6 actions">
-          <a href="/">View</a>
+          <a href="/communities/{{$key->id}}">View</a>
           <a href="/">Delete</a>
         </div>
       </div>
     </div>
+    @endforeach
   </body>
 </html>
