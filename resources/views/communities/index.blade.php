fbaec599097d5031364f51a8127796aa37cc9961
Zane Finner <zanefinnergm@gmail.com>
2020-05-18 14:52:19 -0400

    draft view

---
+++ resources/views/communities/index.blade.php
@@ -0,0 +1,28 @@
+<!doctype HTML>
+<html>
+  <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="/common.css">
+  </head>
+  <body>
+    <div class="container">
+    <h1>Communities</h1>
+    <form action="/communities" method="post">
+        <input type="" placeholder="" name=""/>
+        <input type="" placeholder="" name=""/>
+        <input type="" placeholder="" name=""/>
+        <button type="submit">Add</button>
+    </form>
+      <div class="row">
+        <div class="col-6 info">
+          <h2>Name</h2>
+          <p>Description</p>
+        </div>
+        <div class="col-6 actions">
+          <a href="/">View</a>
+          <a href="/">Delete</a>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
